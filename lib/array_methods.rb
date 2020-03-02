fc1357067c3c4b11dbc415e8162daedece0f14ea
Anthony mendoza <anthonymendoza9922@gmail.com>
2020-03-02 17:36:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,7 +9,8 @@ end
 #-------------------------------------------------------
 
 def find_max_value(array)
-  # Add your solution here
+x = 0 
+array.length.times { |index|
 end
 
 def find_min_value(array)
